1d38592892d3ba5b5ec6ca264c03447a54dfa4b8
MarkMifsud <40730710+MarkMifsud@users.noreply.github.com>
2019-11-04 20:00:21 +0100

    Unit test for ExchangeRate, Util & a method in current manager.  Upgraded pom.xml.

--- currencymanager/src/test/java/edu/uom/currencymanager/CurrencyManagerTest.java
+++ currencymanager/src/test/java/edu/uom/currencymanager/CurrencyManagerTest.java
@@ -1,8 +1,64 @@
-// testing the main program is not UNIT TESTING but this file is created in case any units in this class prove testable
 
 package edu.uom.currencymanager;
+// testing the main program is not UNIT TESTING but this file is created in case any units in this class prove testable
 
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import edu.uom.currencymanager.currencies.Currency;
+import edu.uom.currencymanager.currencies.CurrencyDatabase;
+import edu.uom.currencymanager.currencies.ExchangeRate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import static junit.framework.TestCase.fail;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class  CurrencyManagerTest {
+
+    CurrencyManager myManager;
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void teardown() {
+
+    }
+
+
+    @Test
+    public void testAddCurrencyLongCode() throws Exception {
+        // Throws exception if currency code larger than 3 characters
+        myManager= new CurrencyManager();
+
+        try {
+            myManager.addCurrency("LONGCODE", "LONG Description", true);
+        } catch (Exception e) {
+            assertThat(e.getMessage(), is("A currency code should have 3 characters."));
+        }
+    }
+
+    @Test
+    public void testAddCurrencyShortName() throws Exception {
+        // Throws exception if currency name smaller than 4 characters
+        myManager= new CurrencyManager();
+
+        try {
+            myManager.addCurrency("AAA", "its", true);
+        } catch (Exception e) {
+            assertThat(e.getMessage(), is("A currency's name should be at least 4 characters long."));
+        }
+    }
+
+
+}
+
 
--- currencymanager/src/test/java/edu/uom/currencymanager/currencies/ExchangeRateTest.java
+++ currencymanager/src/test/java/edu/uom/currencymanager/currencies/ExchangeRateTest.java
@@ -1,5 +1,71 @@
+package edu.uom.currencymanager.currencies;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+
+
+public class ExchangeRateTest {
+
+    ExchangeRate myExchangeRate;
+    Currency sourceCurrency= new Currency("JPY","Japanese Yen", true);
+    Currency destinationCurrency= new Currency("NOK","Norweigian Krone", true);
+
+    @Before
+    public void setup() {
+        //Setup
+
+        myExchangeRate = new ExchangeRate(sourceCurrency,destinationCurrency,0.5432);
+    }
+
+    @After
+    public void teardown() {
+        myExchangeRate = null;
+    }
+
+    @Test
+    public void testExchangeRateSourceCurrency()  throws Exception {
+
+        //Verify
+        assertSame (sourceCurrency, myExchangeRate.sourceCurrency);
+
+    }
+
+    @Test
+    public void testExchangeRateDestinationCurrency()  throws Exception {
+
+        //Verify
+        assertSame (destinationCurrency, myExchangeRate.destinationCurrency);
+
+    }
+
+    @Test
+    public void testExchangeRateRate()  throws Exception {
+
+        //Verify
+        assertEquals (0.5432, myExchangeRate.rate,0);
+
+    }
+
+
+    @Test
+    public void testToString()  throws Exception {
+
+        String Result= myExchangeRate.toString();
+        //Verify
+        assertEquals ("JPY 1 = NOK 0.54", Result);
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
