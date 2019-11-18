baf4b5d34212bcbe7ac3ddda5a358139d8d81056
MarkMifsud <40730710+MarkMifsud@users.noreply.github.com>
2019-11-18 19:01:56 +0100

    Assignment 2:  Wasted a whole day trying to make stuff work
    At this point it should be as easy as getting web elements and doing things to them but it is far from that.
    Amazon.co.uk does not just allow you to login, it sends a verification email with a code or asks for a mobile number for 2 factor aouthentication.  It's impossible to just login, so  I tried to work with AliExpress instead.
    
    For some reason there is a uniquely identifiable webElement on the page that it just is Wuoting:  "Unable to find".  WTF?!

--- AmazonTest/src/test/java/AmazonUkStepdefs.java
+++ AmazonTest/src/test/java/AmazonUkStepdefs.java
@@ -1,18 +1,33 @@
-package test.stepdefs;
+package test.stepdefs;  // should be deleted
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import static org.junit.Assert.*;
 
 
 public class AmazonUkStepdefs {
 
+    String text ="";  //any text being read
+
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (Exception e) {}
+    }
+    public boolean loggedIn(){
+        String text= browser.findElement(By.id("2114.best.1000002.i0.6b61J4JbJ4JbUQ")).getText();
+        if (text!="Hi, <b>MT</b>") return false;
+        else return true;
+    }
 
 
         WebDriver browser;
@@ -20,30 +35,32 @@ public class AmazonUkStepdefs {
         @Before
         public void setup(){
             System.setProperty("webdriver.chrome.driver", "C:/users/mark/software/chromedriver.exe");
-            browser = new ChromeDriver();
 
-            browser.get("https://www.amazon.co.uk/"); // go to the site
         }
 
         @After
         public void teardown() {
-            browser.quit();
+            //text="";
+           // browser.quit();
         }
 
 
     // Scenario Valid Login requires the following 3 tests
     @Given("I am a user on the website")
     public void i_am_a_user_on_the_website() {
+
+        browser = new ChromeDriver();
         // The user is not logged in, therefore this module should check logout the user if necessary
         // this test is required by Scanarios: Valid Login, Invalid Login
 
         // USer not logged in is characterised by: <span class="nav-line-1">Hello, Sign in</span>
+        browser.get("https://best.aliexpress.com/"); // go to the site
 
-        String text= browser.findElement(By.className("nav-line-1")).getText();
+        //if (loggedIn()==true) logOut();
 
-        assertEquals("Hello, Sign in", text) ; //browser.getTitle())
+        assertEquals(loggedIn(), false) ;
 
-        //throw new cucumber.api.PendingException();
+
     }
 
     @When("I log in using valid credentials")
@@ -51,6 +68,8 @@ public class AmazonUkStepdefs {
         // Write code here that turns the phrase above into concrete actions
 
         assertEquals(1,1);
+
+
         //throw new cucumber.api.PendingException();
     }
 
@@ -58,8 +77,11 @@ public class AmazonUkStepdefs {
     public void i_should_be_logged_in() {
         // Write code here that turns the phrase above into concrete actions
         assertEquals(1,1);
-
+        //assertEquals(loggedIn(), true) ;
         //throw new cucumber.api.PendingException();
+
+        text="";
+        browser.quit();
     }
     //
 
@@ -67,21 +89,41 @@ public class AmazonUkStepdefs {
     //  Scenario Invalid Login needs the following 2 tests
     @When("I log in using invalid credentials")
     public void i_log_in_using_invalid_credentials() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
+        browser.findElement(By.id("nav-link-accountList")).click(); //find the Sign In button
+        sleep(6);  //give chance for page to laod
+        WebElement found = browser.findElement(By.className("nav-action-button")); //find the Sign In button
+        if (found !=null) browser.findElement(By.className("nav-action-button")).click(); //find the Sign In button
+        sleep(6);  //give chance for page to laod
+        browser.findElement(By.id("ap_email")).sendKeys("an invalid email");
+        //browser.findElement(By.className("a-input-text a-span12 auth-autofocus auth-required-field")).sendKeys("an invalid email");
+
+
+        browser.findElement(By.id("continue")).click();
+
+
+
+        sleep(6);  //give chance for login to be attempted
+        text = browser.findElement(By.className("a-alert-heading")).getText();
+        assertEquals(text,"There was a problem");
     }
 
     @Then("I should not be logged in")
     public void i_should_not_be_logged_in() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
+        assertEquals(text,"There was a problem");
+        // throw new cucumber.api.PendingException();
+
+        text="";
+        browser.quit();
     }
 
 
 
     @Given("I am a logged in user on the website")
     public void i_am_a_logged_in_user_on_the_website() {
-        // Write code here that turns the phrase above into concrete actions
+       // browser = new ChromeDriver();
+
         throw new cucumber.api.PendingException();
     }
 
@@ -100,12 +142,16 @@ public class AmazonUkStepdefs {
     @Then("I should see the product details")
     public void i_should_see_the_product_details() {
         // Write code here that turns the phrase above into concrete actions
+
+        text="";
+        browser.quit();
         throw new cucumber.api.PendingException();
     }
 
     @Given("my shopping cart is empty")
     public void my_shopping_cart_is_empty() {
-        // Write code here that turns the phrase above into concrete actions
+        //browser = new ChromeDriver();
+
         throw new cucumber.api.PendingException();
     }
 
@@ -124,6 +170,9 @@ public class AmazonUkStepdefs {
     @Then("my shopping cart should contain {int} item")
     public void my_shopping_cart_should_contain_item(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
+
+        text="";
+        browser.quit();
         throw new cucumber.api.PendingException();
     }
 
@@ -141,7 +190,8 @@ public class AmazonUkStepdefs {
 
     @Given("my shopping cart has {int} products")
     public void my_shopping_cart_has_products(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
+       // browser = new ChromeDriver();
+
         throw new cucumber.api.PendingException();
     }
 
--- AmazonTest/src/test/java/SeparateTests.java
+++ AmazonTest/src/test/java/SeparateTests.java
@@ -1,5 +1,6 @@
 package test.system.google;
 
+import cucumber.api.java.en.Then;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,16 +17,22 @@ public class SeparateTests {
 
     WebDriver browser;
 
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (Exception e) {}
+    }
+
     @Before
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "C:/users/mark/software/chromedriver.exe");
         browser = new ChromeDriver();
-        browser.get("https://www.amazon.co.uk/");
+        browser.get("https://best.aliexpress.com");
     }
 
     @After
     public void teardown() {
-        browser.quit();
+      //  browser.quit();
     }
 
     @Test
@@ -50,10 +57,64 @@ public class SeparateTests {
     @Test
     public void notLoggedIn(){
 
+        String text= browser.findElement(By.className("account-name")).getText();
 
-        String text= browser.findElement(By.className("nav-line-1")).getText();
+        //String text= browser.findElement(By.className("account-unsigned")).getClass().toString();
 
-        assertEquals("Hello, Sign in", text) ; //browser.getTitle())
+            //assertEquals( "class org.openqa.selenium.remote.RemoteWebElement", text) ;
+        assertEquals( "", text) ;
+
+    }
+
+    @Test
+    public void badLogIn(){
+
+
+        browser.findElement(By.id("nav-link-accountList")).click(); //find the Sign In button
+        sleep(2);  //give chance for page to laod
+        browser.findElement(By.className("nav-action-button")).click(); //find the Sign In button
+        //sleep(6);  //give chance for page to laod
+        browser.findElement(By.id("ap_email")).sendKeys("an invalid email");
+        //browser.findElement(By.className("a-input-text a-span12 auth-autofocus auth-required-field")).sendKeys("an invalid email");
+
+
+        browser.findElement(By.id("continue")).click();
+        sleep(2);  //give chance for login to be attempted
+        String text = browser.findElement(By.className("a-alert-heading")).getText();
+        assertEquals(text,"There was a problem");
+
+
+    }
+
+    @Test
+    public void LoggingIn(){
+
+
+        browser.findElement(By.className("user-account-info")).click(); //find the Sign In button
+        browser.findElement(By.className("sign-btn")).click(); //find the Sign In button
+        sleep(8);  //give chance for page to load
+
+        //enter email
+        browser.findElement(By.className("notice-close")).click(); //find the Sign In button
+        //sleep(6);
+        browser.findElement(By.id("fm-login-id")).sendKeys("realitysight@gmail.com");
+        browser.findElement(By.id("fm-login-password")).sendKeys("WeakPassword123");
+
+        //browser.findElement(By.className("a-icon a-icon-checkbox")).click();
+        //<i class="a-icon a-icon-checkbox"></i>
+
+        browser.findElement(By.className("fm-button fm-submit password-login")).click();
+        sleep(6);  //give chance for login to be attempted
+
+
+        //go back home
+      //  browser.findElement(By.className("a-link-nav-icon")).click();
+
+
+        String text= browser.findElement(By.className("account-name")).getText();
+        assertEquals(text,"Hello, Bogus");
+
+
     }
 
 }
